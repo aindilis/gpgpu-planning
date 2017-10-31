bd6806952cc7453f7d2ed2226839e68d0ef03eb3
Andrew Dougherty <andrewdo@frdcsa.org>
2017-10-31 12:11:09 -0400

    Split merged files.

--- systems/gpuplan/src/StatesContainer.h
+++ systems/gpuplan/src/StatesContainer.h
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 /*
  * StatesContainer.h
  *
@@ -57,35 +56,4 @@ public:
 };
 
 #endif /* STATESCONTAINER_H_ */
-=======
-/*
- * StatesContainer.h
- *
- *  Created on: 06.01.2010
- *      Author: sulewski
- */
 
-#ifndef STATESCONTAINER_H_
-#define STATESCONTAINER_H_
-
-#include "defines.h"
-
-#ifdef BOOLHASH
-#include "bloomHash.h"
-typedef bloomHash<unsigned char *> hashing;
-#endif
-
-class myState;
-
-class StatesContainer {
-private:
-public:
-	virtual unsigned int get(char * dst, unsigned int layer, unsigned int cost, unsigned int bufferSize) = 0 ;  //get maximal bufferSize elements from open list and copy it to dst
-	virtual	unsigned int getNextCosts(unsigned int layer,unsigned int current_costs) = 0;
-	virtual unsigned char * insert(unsigned int bfslayer, myState * state,bool insert) = 0;
-	virtual bool clear() = 0;
-	virtual void printAll() = 0;
-};
-
-#endif /* STATESCONTAINER_H_ */
->>>>>>> .r254
--- systems/gpuplan/src/openList.h
+++ systems/gpuplan/src/openList.h
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 /*
  * openList.h
  *
@@ -30,58 +29,3 @@ class openList{
 		bool close();
 
 };
-=======
-/*
- * openList.h
- *
- * Created on: 18.08.2010
- * 	Author: damian
- */
-
-#ifndef OPENLIST_H_
-#define OPENLIST_H_
-
-#define READBUFFERSIZE 1048576000
-
-//#include "GPUCheck.h"
-#include <map>
-#include "myModel.h"
-#include "string"
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <pthread.h>
-#include <list>
-
-
-template <class T>
-class openList {
-	private:
-		unsigned int elementLength; //length of inserted elements in bytes
-		map<unsigned int,map<unsigned int, unsigned int> * > openElements; // 2 dimensional array containing number of elements in each list
-		map<unsigned int,map<unsigned int, vector<T> *>* > openLists; // 2 dimensional array of lists (depth, costs) // for internal usage
-		map<unsigned int,map<unsigned int, fstream * > * > openFiles; //open lists for external usage
-		map<unsigned int,map<unsigned int, fstream * > * > openFilesRead; //open lists for external usage
-		map<unsigned int,map<unsigned int, filebuf * > * > openBuffers;
-
-    pthread_mutex_t serialize_mutexes;
-		map<unsigned int,pthread_mutex_t * > serialize_open_map;
-
-	public:
-
-		unsigned int elements; //move to private later
-		openList(unsigned int elementLength_);
-
-		bool insert(unsigned int layer, unsigned int cost, T state, unsigned int elementCount = 1); //insert a state into the open list
-		unsigned int get(char * dst, unsigned int layer, unsigned int cost, unsigned int bufferSize);  //get maximal bufferSize elements from open list and copy it to dst
-
-		unsigned int getNextCosts(unsigned int layer,unsigned int current_costs);
-		bool clear(); // remove all elements
-		bool close();
-
-};
-
-#include "openList.cc"
-
-#endif //OPENLIST_H_
->>>>>>> .r254
